9d2247e9de277bfd44419b22b26fd70903cd3586
Alejandro Gómez Martínez <agoma8@gmail.com>
2012-07-02 16:17:06 +0200

    Added bucky and collectd installation and configuration. Changed install.sh bash script and removed pos-install.sh script

--- install.sh
+++ install.sh
@@ -3,52 +3,80 @@
 
 PROJECT_NAME="monitor"
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+USER=www-data
+GROUP="$USER"
 
-apt-get install -q -y $(< apt-requirements.txt)
+sudo apt-get install -q -y $(< apt-requirements.txt)
 
 # In Ubuntu 64bits these libs are not installed in a directory where PIL can find them
-if [ ! -f /usr/lib/libjpeg.so ]; then
-	ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
-	ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
-	ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
+if [[ ! -f /usr/lib/libjpeg.so ]]; then
+	sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
+	sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
+	sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
 fi
 
 # Install nginx configuration
-if [ -d /etc/nginx/sites-enabled ]; then
-	ln -s "${SCRIPT_DIR}/etc/nginx.conf" "/etc/nginx/sites-available/${PROJECT_NAME}-nginx.conf"
-	ln -s "/etc/nginx/sites-available/${PROJECT_NAME}-nginx.conf" "/etc/nginx/sites-enabled/${PROJECT_NAME}-nginx.conf"
-elif [ -d /etc/nginx/conf.d ]; then
-	ln -s "${SCRIPT_DIR}/etc/nginx.conf" "/etc/nginx/conf.d/${PROJECT_NAME}-nginx.conf"
+if [[ -d /etc/nginx/sites-enabled ]]; then
+	sudo ln -s "${SCRIPT_DIR}/etc/nginx.conf" "/etc/nginx/sites-available/${PROJECT_NAME}-nginx.conf"
+	sudo ln -s "/etc/nginx/sites-available/${PROJECT_NAME}-nginx.conf" "/etc/nginx/sites-enabled/${PROJECT_NAME}-nginx.conf"
+elif [[ -d /etc/nginx/conf.d ]]; then
+	sudo ln -s "${SCRIPT_DIR}/etc/nginx.conf" "/etc/nginx/conf.d/${PROJECT_NAME}-nginx.conf"
 else
 	echo "Install nginx before you run this script (directory /etc/nginx/sites-enabled or /etc/nginx/conf.d doesn't exist)."
 	exit 1
 fi
 
+sudo mkdir -p /opt/var/log /opt/var/pid /opt/graphite /opt/envs
+sudo chown "$USER":"$GROUP" /opt/var /opt/var/log /opt/var/pid /opt/var/pid /opt/graphite /opt/envs
+
+if [[ ! -d "/opt/envs/${PROJECT_NAME}" ]]; then
+        virtualenv --distribute "/opt/envs/${PROJECT_NAME}"
+fi
+source "/opt/envs/${PROJECT_NAME}/bin/active"
+
+if [[ ! -f "${SCRIPT_DIR}/requirements.txt" ]]; then
+        pip install -r "${SCRIPT_DIR}/requirements.txt"
+fi
+
+ln -s /opt/monitor/etc/graphite/{carbon.conf,storage-schemas.conf} /opt/graphite/conf/
+ln -s /opt/monitor/etc/graphite/local_settings.py /opt/graphite/webapp/graphite/
+
+# Installs the projects' collectd config, if it has any
+if [[ -d /etc/collectd && -f "${SCRIPT_DIR}/etc/collectd.conf" ]]; then
+	sudo ln -sf "${SCRIPT_DIR}/etc/collectd.conf" "/etc/collectd/collectd.conf"
+        sudo /etc/init.d/collectd restart
+fi
+
+echo "Enter the MySQL password of root"
+mysql -u root -p -e "CREATE DATABASE graphite"
+/opt/graphite/webapp/graphite/manage.py syncdb
+
+sudo apt-get install supervisor
 # Install supervisor processes
 if [ -d /etc/supervisor/conf.d ]; then
 	for fn in "${SCRIPT_DIR}"/etc/supervisor/*.conf; do
 		base_fn=$(basename $fn)
-		ln -s "$fn" "/etc/supervisor/conf.d/${PROJECT_NAME}-${base_fn}"
+		sudo ln -s "$fn" "/etc/supervisor/conf.d/${PROJECT_NAME}-${base_fn}"
 	done
 else
 	echo "Install supervisor before you run this script (directory /etc/supervisor/conf.d doesn't exist)."
 fi
 
+sudo supervisorctl -c /etc/supervisor/supervisord.conf reread "${PROJECT_NAME}"
+# TODO: Verify if we need update. I.e., does restart takes care of update?
+sudo supervisorctl -c /etc/supervisor/supervisord.conf update "${PROJECT_NAME}":*
+sudo supervisorctl -c /etc/supervisor/supervisord.conf restart "${PROJECT_NAME}":*
+
 # Installs the projects' crontab, if it has any
-if [ -d /etc/cron.d -a -f "${SCRIPT_DIR}/etc/crontab" ]; then
-	ln -s "${SCRIPT_DIR}/etc/crontab" "/etc/cron.d/${PROJECT_NAME}-crontab"
+if [[ -d /etc/cron.d && -f "${SCRIPT_DIR}/etc/crontab" ]]; then
+	sudo ln -s "${SCRIPT_DIR}/etc/crontab" "/etc/cron.d/${PROJECT_NAME}-crontab"
 fi
 
 # Install the Microsoft Windows Vista fonts
 if [ ! -d /usr/share/fonts/vista ]; then
 	wget http://cuenca-stuff.s3.amazonaws.com/install/vista-fonts.zip
-	unzip vista-fonts.zip -d /usr/share/fonts
-	mv /usr/share/fonts/Fonts /usr/share/fonts/vista
+	sudo unzip vista-fonts.zip -d /usr/share/fonts
+	sudo mv /usr/share/fonts/Fonts /usr/share/fonts/vista
 	rm vista-fonts.zip
-	fc-cache -vf
+	sudo fc-cache -vf
 fi
-
-mkdir -p /opt/var/log /opt/var/pid /opt/graphite
-chown www-data:www-data /opt/var /opt/var/log /opt/var/pid /opt/var/pid
-
-# mkvirtualenv -r /opt/monitor/requirements.txt monitor
--- post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# You should run this script as your regular user
-pip install -r requirements.txt
-ln -s /opt/monitor/etc/graphite/{carbon.conf,storage-schemas.conf} /opt/graphite/conf/
-ln -s /opt/monitor/etc/graphite/local_settings.py /opt/graphite/webapp/graphite/
-echo "Enter the MySQL password of root"
-mysql -u root -p -e "CREATE DATABASE graphite"
-python /opt/graphite/webapp/graphite/manage.py syncdb
-sudo chown -R www-data.www-data /opt/graphite
